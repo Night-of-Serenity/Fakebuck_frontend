9f132745edd65c40152448b4a047d61bcf5305d1
Nightz <peerapat.ud@gmail.com>
2023-05-29 16:24:42 +0700

    create async onSummit and dispatch on loginForm

--- src/features/auth/slice/auth-slice..js
+++ src/features/auth/slice/auth-slice..js
@@ -8,14 +8,22 @@ const initialState = {
   loading: false,
 };
 
-export const registerAsync = createAsyncThunk("auth/registerAsync", async (input, thunkApi) => {
-  try {
-    const res = await authService.register(input);
-    setAccessToken(res.data.accessToken);
-    return;
-  } catch (err) {
-    return thunkApi.rejectWithValue(err.response.data.message);
+export const registerAsync = createAsyncThunk(
+  "auth/registerAsync",
+  async (input, thunkApi) => {
+    try {
+      const res = await authService.register(input);
+      setAccessToken(res.data.accessToken);
+      return;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err.response.data.message);
+    }
   }
+);
+
+export const login = createAsyncThunk("auth/login", async (input) => {
+  const res = await authService.login(input);
+  setAccessToken(res.data.accessToken);
 });
 
 const authSlice = createSlice({
@@ -38,6 +46,9 @@ const authSlice = createSlice({
       .addCase(registerAsync.rejected, (state, action) => {
         state.error = action.payload;
         state.loading = false;
+      })
+      .addCase(login.fulfilled, (state) => {
+        state.isAuthenticated = true;
       }),
 });
 
