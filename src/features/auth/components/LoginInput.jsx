5a1c5eae6ef7ab6cace30b090b42d74c5e8ad429
Nightz <peerapat.ud@gmail.com>
2023-05-29 15:27:49 +0700

    make validattion for loginform

--- src/features/auth/components/LoginInput.jsx
+++ src/features/auth/components/LoginInput.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import createClassName from "../../../utils/create-classes";
 
-export default function LoginInput({ placeholder }) {
+export default function LoginInput({
+  placeholder,
+  value,
+  onChange,
+  name,
+  isInvalid,
+}) {
+  const className = createClassName(
+    "block w-full border rounded-md px-4 py-3.5 outlin-none focus:ring-1",
+    isInvalid
+      ? "border-red-500 focus:ting-red-300"
+      : "border-gray-300 focus:border-blue-500 focus:ring-blue-300"
+  );
   return (
     <input
-      className="block w-full border border-gray-300 rounded-md px-4 py-3.5 outline-none focus:border-blue-500 focus:ring- focus:ring-1 focus:ring-blue-300"
+      type="text"
+      className={className}
       placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      name={name}
     />
   );
 }
