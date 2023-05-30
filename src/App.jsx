f38b820ec8715b39aad8b4f976e893d6a0458198
Nightz <peerapat.ud@gmail.com>
2023-05-30 15:33:28 +0700

    make loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Loading from "./components/Loading";
 import Router from "./route/Router";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
+      <Loading />
       <Router />;
       <ToastContainer position="bottom-center" theme="dark" autoClose={3000} />
     </div>
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import { LoaderIcon } from "../icons";
 
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <>
+      <div className="fixed inset-0 bg-black opacity-25 z-40"></div>
+      <div className="fixed inset-0 z-50">
+        <div className="flex justify-center items-center min-h-full">
+          <LoaderIcon className="fill-blue-300 animate-spin" />
+        </div>
+      </div>
+    </>
+  );
 }
