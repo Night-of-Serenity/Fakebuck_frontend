c3393697a77f1dac0f9aab72383792e203fb8be7
Nightz <peerapat.ud@gmail.com>
2023-05-31 10:11:47 +0700

    make edit input image file for editprofile

--- src/features/profile/components/EditProfileForm.jsx
+++ src/features/profile/components/EditProfileForm.jsx
@@ -4,22 +4,27 @@ import PictureForm from "./PictureForm";
 export default function EditProfileForm() {
   return (
     <div className="flex flex-col gap-4">
-      <PictureForm title="Profile Image">
-        <div className="flex justify-center">
-          <Avatar
-            className="h-[10.5rem] w-[10.5rem]"
-            alt="user"
-            src="https://images.pexels.com/photos/1267335/pexels-photo-1267335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          />
-        </div>
+      <PictureForm
+        // onSave={()=>{saveCover}}
+        title="Profile Image"
+        initialSrc="https://images.pexels.com/photos/1267335/pexels-photo-1267335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      >
+        {(src) => (
+          <div className="flex justify-center">
+            <Avatar className="h-[10.5rem] w-[10.5rem]" alt="user" src={src} />
+          </div>
+        )}
       </PictureForm>
-      <PictureForm title="Cover Image">
-        <div className="aspect-[1096/404] overflow-hidden flex justify-center items-center rounded-lg">
-          <img
-            alt="cover"
-            src="https://images.pexels.com/photos/957002/berchtesgaden-alpine-watzmann-berchtesgaden-national-park-957002.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          />
-        </div>
+      <PictureForm
+        // onSave={()=>{saveCover}}
+        title="Cover Image"
+        initialSrc="https://images.pexels.com/photos/957002/berchtesgaden-alpine-watzmann-berchtesgaden-national-park-957002.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      >
+        {(src) => (
+          <div className="aspect-[1096/404] overflow-hidden flex justify-center items-center rounded-lg">
+            <img alt="cover" src={src} />
+          </div>
+        )}
       </PictureForm>
     </div>
   );
--- src/features/profile/components/PictureForm.jsx
+++ src/features/profile/components/PictureForm.jsx
@@ -1,17 +1,43 @@
+import { useRef } from "react";
 import FormButton from "./FormButton";
+import { useState } from "react";
 
-export default function PictureForm({ title, children }) {
+export default function PictureForm({ title, children, initialSrc, onSave }) {
+  const inputEl = useRef();
+  const [file, setFile] = useState(null);
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
+        <input
+          type="file"
+          className="hidden"
+          ref={inputEl}
+          onChange={(e) => {
+            // console.log(e.target.value);
+            if (e.target.files[0]) {
+              setFile(e.target.files[0]);
+            }
+          }}
+        ></input>
         <h5 className="text-xl font-bold">{title}</h5>
         <div>
-          <FormButton>Save</FormButton>
-          <FormButton>Cancel</FormButton>
-          <FormButton>Edit</FormButton>
+          {file && (
+            <>
+              <FormButton onClick={onSave}>Save</FormButton>
+              <FormButton
+                onClick={() => {
+                  setFile(null);
+                  inputEl.current.value = "";
+                }}
+              >
+                Cancel
+              </FormButton>
+            </>
+          )}
+          <FormButton onClick={() => inputEl.current.click()}>Edit</FormButton>
         </div>
       </div>
-      {children}
+      {children(file ? URL.createObjectURL(file) : initialSrc)}
     </div>
   );
 }
