a265c43a743841897fdf67cf54991cc9c3aa9ee4
Nightz <peerapat.ud@gmail.com>
2023-06-02 12:29:19 +0700

    make create post feature

--- src/features/post/components/CreatePostBox.jsx
+++ src/features/post/components/CreatePostBox.jsx
@@ -1,10 +1,16 @@
 import { useSelector } from "react-redux";
 import Avatar from "../../../components/Avatar";
 import { Link } from "react-router-dom";
+import Modal from "../../../components/Modal";
+import { useState } from "react";
+import PostForm from "./PostForm";
+
 export default function CreatePostBox() {
+  const [open, setOpen] = useState(false);
   const { id, firstName, profileImage } = useSelector(
     (state) => state.auth.user
   );
+
   return (
     <div className="bg-white rounded-lg border shadow px-4 py-3 flex gap-2">
       <div>
@@ -13,11 +19,20 @@ export default function CreatePostBox() {
         </Link>
       </div>
       <div
-        className="rounded-full bg-gray-200 text-gray-500 px-3 py-1.5 hover:bg-gray-300"
+        className="rounded-full bg-gray-200 text-gray-500 px-3 py-1.5 hover:bg-gray-300 flex-1"
         role="button"
+        onClick={() => setOpen(true)}
       >
-        What&rsquo;s on your mind,{firstName}?
+        What&rsquo;s on your mind, {firstName}?
       </div>
+      <Modal
+        title="Create post"
+        width={32}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <PostForm />
+      </Modal>
     </div>
   );
 }
